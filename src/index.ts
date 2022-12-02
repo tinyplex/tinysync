c1d28cd48f019620ade5dc715b41e14e8950f854
James Pearce <github@tripleodeon.com>
2022-12-02 09:49:22 -0500

    Pretend server
    ```
    Store 1 {
      pets: {
        fido: { species: 'dog', price: 5 },
        felix: { species: 'cat', furry: true },
        rex: { species: 'dog' }
      }
    } [
      [ '1669992522998,0001,store1', 'pets', 'fido', 'species', 'dog' ],
      [ '1669992522998,0002,store1', 'pets', 'felix', 'species', 'cat' ],
      [ '1669992522998,0003,store1', 'pets', 'felix', 'legs', 4 ],
      [ '1669992522998,0004,store1', 'pets', 'felix', 'furry', true ],
      [ '1669992522998,0005,store1', 'pets', 'rex', 'species', 'dog' ],
      [ '1669992522998,0006,store1', 'pets', 'fido', 'price', 5 ],
      [ '1669992522998,0007,store1', 'pets', 'felix', 'legs', undefined ]
    ]
    Store 2 {
      pets: { cujo: { species: 'wolf' }, felix: { species: 'cat', purrs: true } }
    } [
      [ '1669992523002,0001,store2', 'pets', 'cujo', 'species', 'wolf' ],
      [ '1669992523002,0002,store2', 'pets', 'felix', 'species', 'cat' ],
      [ '1669992523002,0003,store2', 'pets', 'felix', 'purrs', true ]
    ]
    Server [
      [ '1669992522998,0001,store1', 'pets', 'fido', 'species', 'dog' ],
      [ '1669992522998,0002,store1', 'pets', 'felix', 'species', 'cat' ],
      [ '1669992522998,0003,store1', 'pets', 'felix', 'legs', 4 ],
      [ '1669992522998,0004,store1', 'pets', 'felix', 'furry', true ],
      [ '1669992522998,0005,store1', 'pets', 'rex', 'species', 'dog' ],
      [ '1669992522998,0006,store1', 'pets', 'fido', 'price', 5 ],
      [ '1669992522998,0007,store1', 'pets', 'felix', 'legs', undefined ],
      [ '1669992523002,0001,store2', 'pets', 'cujo', 'species', 'wolf' ],
      [ '1669992523002,0002,store2', 'pets', 'felix', 'species', 'cat' ],
      [ '1669992523002,0003,store2', 'pets', 'felix', 'purrs', true ]
    ]
    Store 3 {
      pets: {
        fido: { species: 'dog', price: 5 },
        felix: { species: 'cat', furry: true, purrs: true },
        rex: { species: 'dog' },
        cujo: { species: 'wolf' }
      }
    }
    ```

--- src/index.ts
+++ src/index.ts
@@ -1,25 +1,32 @@
 /* eslint-disable no-console */
+import {CellChanges, createSync} from './sync';
 import {createStore} from 'tinybase/store';
-import {createSync} from './sync';
+
+const server: CellChanges = [];
 
 const store1 = createStore();
 const sync1 = createSync(store1, 'store1');
-
 store1.setTables({
-  pets: {fido: {species: 'dog'}, felix: {species: 'cat', legs: 4}},
+  pets: {fido: {species: 'dog'}, felix: {species: 'cat', legs: 4, furry: true}},
 });
 store1.setRow('pets', 'rex', {species: 'dog'});
 store1.setCell('pets', 'fido', 'price', 5);
 store1.delCell('pets', 'felix', 'legs');
-
-console.log('Original store:', store1.getTables());
+const cellChanges1 = sync1.getCellChanges();
+server.push(...cellChanges1);
+console.log('Store 1', store1.getTables(), cellChanges1);
 
 const store2 = createStore();
 const sync2 = createSync(store2, 'store2');
+store2.setTables({
+  pets: {cujo: {species: 'wolf'}, felix: {species: 'cat', purrs: true}},
+});
+const cellChanges2 = sync2.getCellChanges();
+server.push(...cellChanges2);
+console.log('Store 2', store2.getTables(), cellChanges2);
 
-const cellChanges = sync1.getCellChanges();
-console.log('Cell changes', cellChanges);
-
-sync2.applyCellChanges(cellChanges);
-
-console.log('Synced store:', store2.getTables());
+const store3 = createStore();
+const sync3 = createSync(store3, 'store3');
+sync3.applyCellChanges(server);
+console.log('Server', server);
+console.log('Store 3', store3.getTables());
--- src/sync.ts
+++ src/sync.ts
@@ -12,26 +12,39 @@ export type CellChange = [
 export type CellChanges = CellChange[];
 
 export const createSync = (store: Store, uniqueStoreId: Id) => {
+  let listening = 1;
   const cellChanges: CellChange[] = [];
 
   const getHlc = getHlcFunction(uniqueStoreId);
-  const cellChanged = (
-    _store,
-    tableId: Id,
-    rowId: Id,
-    cellId: Id,
-    newCell: CellOrUndefined,
-  ) => cellChanges.push([getHlc(), tableId, rowId, cellId, newCell]);
 
-  store.addCellListener(null, null, null, cellChanged);
-  const sync = {
-    getCellChanges: () => cellChanges,
-    applyCellChanges: (cellChanges: CellChanges) =>
+  const _listenerId = store.addCellListener(
+    null,
+    null,
+    null,
+    (_store, tableId: Id, rowId: Id, cellId: Id, newCell: CellOrUndefined) => {
+      if (listening) {
+        cellChanges.push([getHlc(), tableId, rowId, cellId, newCell]);
+      }
+    },
+  );
+
+  const getCellChanges = () => cellChanges;
+
+  const applyCellChanges = (cellChanges: CellChanges) => {
+    listening = 0;
+    store.transaction(() =>
       cellChanges.forEach(([_time, tableId, rowId, cellId, newCell]) =>
         newCell == undefined
           ? store.delCell(tableId, rowId, cellId)
           : store.setCell(tableId, rowId, cellId, newCell),
       ),
+    );
+    listening = 1;
+  };
+
+  const sync = {
+    getCellChanges,
+    applyCellChanges,
   };
 
   return Object.freeze(sync);
